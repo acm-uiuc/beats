56d2d9c968a1836208dfdfd5ea4f21574a3a32d2
Zach Reizner <zach297@gmail.com>
2014-02-27 19:16:02 -0600

    initial commit

---
+++ beats.js
@@ -0,0 +1,50 @@
+function makeSong(title, albumTitle, trackNum, artist, length)
+{
+    return {
+        title:      title,
+        albumTitle: albumTitle,
+        trackNum:   trackNum,
+        artist:     artist,
+        length:     length,
+    };
+}
+
+function BeatsController($scope, $http)
+{
+    $scope.sections =
+    [
+        { title: 'Queue', icon: '\uf03a' },
+        { title: 'Recently Added', icon: '\uf017' },
+        { title: 'Recently Played', icon: '\uf04b' },
+        { title: 'Random', icon: '\uf074' },
+        { title: 'Top 100', icon: '\uf01b' },
+    ];
+
+    var songs =
+    [
+        makeSong('Waltz of the Flowers', 'The Nutcraker', 8, 'Tchaikovsky', 410),
+        makeSong('March of the Mephisto', 'The Black Halo', 1, 'Kamelot', 329),
+    ];
+
+    for (var i = 0; i < 100; i++)
+    {
+        break;
+        songs[i] = songs[0];
+        songs[i].vote = true;
+    }
+
+    $scope.playlists =
+    [
+        { title: 'Rock' },
+        { title: 'Pop' },
+        { title: 'Top 40' },
+        { title: 'Hardcore' },
+        { title: 'Witch-Hop' },
+    ];
+
+    $scope.playlist = songs;
+
+    $scope.volumePercentage = 0.5;
+    $scope.playbackTime = 50;
+    $scope.playbackDuration = 100;
+};
