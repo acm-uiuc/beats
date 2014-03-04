f8606e2bf85eebda20044f1abda704173942ba12
Zach Reizner <zach297@gmail.com>
2014-03-03 22:38:44 -0600

    start backend interface
    - add search
    - add queue

--- beats.js
+++ beats.js
@@ -1,3 +1,98 @@
+angular.module('moment', [])
+// A filter that takes a number of seconds and converts it to MM:SS format
+.filter('momentFormat', function()
+{
+    return function(input)
+    {
+        var seconds = (Math.floor(input) % 60);
+        if (seconds < 10)
+        {
+            seconds = "0" + seconds;
+        }
+        return Math.floor(input / 60) + ":" + seconds;
+    };
+});
+
+angular.module('BeatsApp', ['moment'])
+.controller('BeatsController', ['$scope', '$http', '$interval', function($scope, $http, $interval)
+{
+    $scope.playlist = [];
+    $scope.queue = [];
+    $scope.volumePercentage = 0.5;
+    $scope.playbackTime = 0;
+    $scope.playbackDuration = 100;
+
+    $scope.sections =
+    [
+        { title: 'Queue', icon: '\uf03a' },
+        { title: 'Recently Added', icon: '\uf017' },
+        { title: 'Recently Played', icon: '\uf04b' },
+        { title: 'Random', icon: '\uf074' },
+        { title: 'Top 100', icon: '\uf01b' },
+    ];
+
+    $scope.playlists =
+    [
+        { title: 'Rock' },
+        { title: 'Pop' },
+        { title: 'Top 40' },
+        { title: 'Hardcore' },
+        { title: 'Witch-Hop' },
+    ];
+
+    $scope.searchSongs = function(query)
+    {
+        $http.get('/v1/songs/search',
+        {
+            params: { 'q': query }
+        })
+        .success(function(data)
+        {
+            var songs = [];
+            for (var resultIndex = 0; resultIndex < data.results.length; resultIndex++)
+            {
+                var result = data.results[resultIndex];
+                songs[resultIndex] = result;
+            }
+            $scope.playlist = songs;
+        });
+    }
+
+    $scope.voteSong = function(song)
+    {
+        console.log(song);
+        $http.post('/v1/queue/add', 'id=' + song._id,
+        {
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+        })
+        .success(function(data)
+        {
+            console.log(data);
+        });
+    };
+
+    $scope.skipSong = function()
+    {
+        $http.post('/v1/player/play_next');
+    };
+
+    $interval(function()
+    {
+        $http.get('/v1/player/status')
+        .success(function(data)
+        {
+            $scope.playbackTime = data['current_time'] / 1000;
+            $scope.playbackDuration = data['duration'] / 1000;
+        });
+
+        $http.get('/v1/queue')
+        .success(function(data)
+        {
+            $scope.queue = data['queue'].slice(data['position']);
+        });
+    }, 1000);
+}]);
+
 function makeSong(title, albumTitle, trackNum, artist, length)
 {
     return {
@@ -9,42 +104,3 @@ function makeSong(title, albumTitle, trackNum, artist, length)
     };
 }
 
-function BeatsController($scope, $http)
-{
-    $scope.sections =
-    [
-        { title: 'Queue', icon: '\uf03a' },
-        { title: 'Recently Added', icon: '\uf017' },
-        { title: 'Recently Played', icon: '\uf04b' },
-        { title: 'Random', icon: '\uf074' },
-        { title: 'Top 100', icon: '\uf01b' },
-    ];
-
-    var songs =
-    [
-        makeSong('Waltz of the Flowers', 'The Nutcraker', 8, 'Tchaikovsky', 410),
-        makeSong('March of the Mephisto', 'The Black Halo', 1, 'Kamelot', 329),
-    ];
-
-    for (var i = 0; i < 100; i++)
-    {
-        songs[i + 2] = JSON.parse(JSON.stringify(songs[0]));
-        songs[i + 2].trackNum = Math.floor(Math.random() * 20) + 1;
-        songs[i + 2].vote = (Math.random() < 0.5);
-    }
-
-    $scope.playlists =
-    [
-        { title: 'Rock' },
-        { title: 'Pop' },
-        { title: 'Top 40' },
-        { title: 'Hardcore' },
-        { title: 'Witch-Hop' },
-    ];
-
-    $scope.playlist = songs;
-
-    $scope.volumePercentage = 0.5;
-    $scope.playbackTime = 50;
-    $scope.playbackDuration = 100;
-};
