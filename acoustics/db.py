7f437f1a9ff13581c1bf2c635adddfad7ec15586
Kevin Wang <kevin@kevinwang.com>
2014-03-05 16:06:53 -0600

    Add database host and port to config file.

--- acoustics/db.py
+++ acoustics/db.py
@@ -1,5 +1,9 @@
 from pymongo import MongoClient
+from config import config
 
-client = MongoClient()
+DB_HOST = config.get('Database', 'host')
+DB_PORT = int(config.get('Database', 'port'))
+
+client = MongoClient(DB_HOST, DB_PORT)
 db = client.acoustics
 songs = db.songs
