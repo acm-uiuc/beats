ca1b2651ed9e3aa374f02b0e4ba03ac33759b4bb
Kevin Wang <kevin@kevinwang.com>
2014-03-06 19:38:11 -0600

    Support CORS preflight requests.

--- acoustics/crossdomain.py
+++ acoustics/crossdomain.py
@@ -43,5 +43,6 @@ def crossdomain(origin=None, methods=None, headers=None,
             return resp
 
         f.provide_automatic_options = False
+        f.required_methods = ['OPTIONS']
         return update_wrapper(wrapped_function, f)
     return decorator
