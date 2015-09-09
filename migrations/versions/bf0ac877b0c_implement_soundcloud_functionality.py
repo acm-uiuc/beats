d240adb5380f816d7485b7905b8784ba9827fbbf
Andrew Yang <andrewyang96@gmail.com>
2015-09-08 22:56:26 -0500

    Update migration script

--- migrations/versions/bf0ac877b0c_implement_soundcloud_functionality.py
+++ migrations/versions/bf0ac877b0c_implement_soundcloud_functionality.py
@@ -17,12 +17,17 @@ import sqlalchemy as sa
 def upgrade():
     op.add_column('packets', sa.Column('soundcloud_url', sa.String(length=100)))
     op.add_column('packets', sa.Column('soundcloud_id', sa.String(length=32)))
-    op.add_column('packets', sa.Column('art_uri', sa.String(length=32)))
+    op.add_column('packets', sa.Column('stream_id', sa.String(length=32)))
+    op.add_column('packets', sa.Column('art_uri', sa.String(length=100)))
     op.add_column('packets', sa.Column('artist', sa.Unicode(length=100)))
     # change column sizes
     op.alter_column('songs', 'title', type_=sa.Unicode(length=200), existing_type=sa.Unicode(length=100))
     op.alter_column('songs', 'artist', type_=sa.Unicode(length=200), existing_type=sa.Unicode(length=100))
     op.alter_column('songs', 'album', type_=sa.Unicode(length=200), existing_type=sa.Unicode(length=100))
+    # rename columns
+    op.alter_column('packets', 'video_url', new_column_name='stream_url')
+    op.alter_column('packets', 'video_title' new_column_name='stream_title')
+    op.alter_column('packets', 'video_length', new_column_name='stream_length')
 
 
 def downgrade():
@@ -34,3 +39,7 @@ def downgrade():
     op.alter_column('songs', 'title', type_=sa.Unicode(length=100), existing_type=sa.Unicode(length(length=200)))
     op.alter_column('songs', 'artist', type_=sa.Unicode(length=100), existing_type=sa.Unicode(length(length=200)))
     op.alter_column('songs', 'album', type_=sa.Unicode(length=100), existing_type=sa.Unicode(length(length=200)))
+    # revert renames
+    op.alter_column('packets', 'stream_url', new_column_name='video_url')
+    op.alter_column('packets', 'stream_title', new_column_name='video_title')
+    op.alter_column('packets', 'stream_length', new_column_name='video_length')
