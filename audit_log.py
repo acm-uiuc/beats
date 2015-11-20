50c8fcb537e471c12b531fea55d10565caee496e
Abhishek Modi <abhishek.modi95@gmail.com>
2015-11-20 01:43:56 -0600

    fixed tabs

--- audit_log.py
+++ audit_log.py
@@ -5,8 +5,8 @@ PLAYER_NAME = config.get('Player', 'player_name')
 LOGGING_ENABLED = config.get('Logging', 'enabled')
 
 def log(user, message):
-	if not LOGGING_ENABLED:
-		return
+    if not LOGGING_ENABLED:
+        return
     session = Session()
     message = AuditLogMessage(
             user=user, message=message, player_name=PLAYER_NAME)
